581d380bb263a94de9882e9909f59abd17f413a1
Hocnchar0vKirill <kirill2021honc@gmail.com>
2023-03-31 11:30:15 +0300

    ...

---
+++ script.js
@@ -0,0 +1,6 @@
+let amount = prompt("Please enter amount(in myles)");
+function convertToKilometres(amount){
+    return amount * 1.60934;
+}
+convertToKilometres();
+console.log(convertToKilometres(parseFloat(amount)));
